bcbca5e8bcf3385128481942e94fa7354c716fae
Imobach González Sosa <igonzalezsosa@suse.com>
2017-06-09 15:35:25 +0100

    Enable most of disabled tests

--- test/AutoinstFunctions_test.rb
+++ test/AutoinstFunctions_test.rb
@@ -2,20 +2,12 @@
 
 require_relative "test_helper"
 
-# storage-ng
-=begin
 Yast.import "AutoinstFunctions"
 Yast.import "Stage"
 Yast.import "Mode"
 Yast.import "AutoinstConfig"
-=end
 
 describe "Yast::AutoinstFunctions" do
-  # storage-ng
-  before :all do
-    skip("pending of storage-ng")
-  end
-
   subject { Yast::AutoinstFunctions }
 
   let(:stage) { "initial" }
